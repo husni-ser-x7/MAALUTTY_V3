082b9323649244d7dddd317b1f478ac8b5ea6ba9
Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-10-15 04:34:32 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,15 +11,15 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/yCYYyEJ.jpeg";
+  r_text[0] = "https://i.imgur.com/26igeri.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/919895828468*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Rafi*
+*owner number wa.me/919605741366*
 
 *Bot making video : https://youtu.be/fd0KPblop-k*
 
