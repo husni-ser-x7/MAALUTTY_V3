a73b11cf2a816af68ad25ec0c041b7d88ea6711c
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-10-22 00:34:05 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -20,10 +20,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Rafi*
 *To check update .update   To update Bot .update now*
+
 *𝐆𝐢𝐭𝐡𝐮𝐛 𝐋𝐢𝐧𝐤 : https://github.com/Luciferking1/Miss-Helenaa*
+    
+       ♡︎           ♡︎          ♡︎
+        
 *𝐀𝐮𝐝𝐢𝐨 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 : https://github.com/Luciferking1/Miss-Helenaa/tree/master/uploads*
+
+       ♡︎           ♡︎          ♡︎
+
 *𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 : https://github.com/Luciferking1/Miss-Helenaa/tree/master/Amalser*
     
+       ♡︎           ♡︎          ♡︎         
+
         ⚠️𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐈𝐬 𝐀 𝐅𝐨𝐫𝐤 𝐀𝐧𝐝 𝐌𝐨𝐝𝐞𝐝 𝐁𝐨𝐭 𝐅𝐫𝐨𝐦 𝐀𝐦𝐚𝐥𝐬𝐞𝐫_𝐕2
 `}) 
 
