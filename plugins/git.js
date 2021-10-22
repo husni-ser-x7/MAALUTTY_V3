c32cd133d5cbfb7adc492d22379c71029f2e1a7f
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-10-23 03:19:58 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -14,7 +14,7 @@ asena.addCommand({pattern: 'git', fromMe: true, deleteCommand: true, desc: GM,},
 
     var r_text = new Array ();
 
-    r_text[1] = "💖*To check update .update✅\nTo update Bot .update now✅*\n\n🎗️*𝐆𝐢𝐭𝐡𝐮𝐛 𝐋𝐢𝐧𝐤 : https://github.com/Luciferking1/Miss-Helenaa✅*\n\n🎗️*𝐀𝐮𝐝𝐢𝐨 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 : https://github.com/Luciferking1/Miss-Helenaa/tree/master/uploads*✅\n\n*🎗️𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 : https://github.com/Luciferking1/Miss-Helenaa/tree/master/Amalser✅*\n\n\n⚠️𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐈𝐬 𝐀 𝐅𝐨𝐫𝐤 𝐀𝐧𝐝 𝐌𝐨𝐝𝐞𝐝 𝐁𝐨𝐭 𝐅𝐫𝐨𝐦 𝐀𝐦𝐚𝐥𝐬𝐞𝐫_𝐕2⚠️";
+   /* r_text[1] = "💖*To check update .update✅\nTo update Bot .update now✅*\n\n🎗️*𝐆𝐢𝐭𝐡𝐮𝐛 𝐋𝐢𝐧𝐤 : https://github.com/Luciferking1/Miss-Helenaa✅*\n\n🎗️*𝐀𝐮𝐝𝐢𝐨 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 : https://github.com/Luciferking1/Miss-Helenaa/tree/master/uploads*✅\n\n*🎗️𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 : https://github.com/Luciferking1/Miss-Helenaa/tree/master/Amalser✅*\n\n\n⚠️𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐈𝐬 𝐀 𝐅𝐨𝐫𝐤 𝐀𝐧𝐝 𝐌𝐨𝐝𝐞𝐝 𝐁𝐨𝐭 𝐅𝐫𝐨𝐦 𝐀𝐦𝐚𝐥𝐬𝐞𝐫_𝐕2⚠️"; */
 
     var i = Math.floor(2*Math.random())
 
