a53640148803c3ded4a9dbe7fe6cd91f17d844b3
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-10-29 01:02:02 +0530

    Update reels.js

--- plugins/reels.js
+++ plugins/reels.js
@@ -17,13 +17,13 @@ Asena.addCommand({pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from i
     var reply = await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
     
    const { status, type, data } = await instagram(match[1], 'julie')
-    if (!status) return await message.sendMessage('✅️Example : https://www.instagram.com/p/CCdcH3FBd1a/?utm_medium=copy_link\n\n*Change /reel/ to /p/ Then give Command .insta*\n\n*Any doubt ask to Farhan or Amal*')
+    if (!status) return await message.sendMessage('✅️Example:-\n\n*Change /reel/ to /p/ Then give Command .insta*\n\n*Any doubt ask to Rafi or Ameen*')
 
     reply = await message.client.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { quoted: message.data });
     
-    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "*Codded by JulieMwol*", quoted: message.data })
+    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "*Codded by Miss Helena*", quoted: message.data })
     
-    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "*Codded by JulieMwol*", quoted: message.data })
+    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "*Codded by Miss Helena*", quoted: message.data })
     
 });
     
