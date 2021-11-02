c4b54e584969a5bd0bd3acdef1e9801683c0f195
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-11-02 13:35:48 +0530

    Update StringSession.js

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -1,8 +1,6 @@
 /* Copyright (C) 2020 Yusuf Usta.
-
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
 */
 
@@ -13,7 +11,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('JULIE_SESSION' in process.env && string === undefined) {
+        if ('AMALSER_CODE' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -21,14 +19,14 @@ class StringSession {
             }
         }
         
-        var split = string.split(':::');
+        var split = string.split(';;;');
         if (split.length >= 2) {
             return JSON.parse(Buffer.from(split[split.length - 1], 'base64').toString('utf-8'));
         }
     }
 
     createStringSession(dict) {
-        return 'JulieMwol:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Amalser;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
