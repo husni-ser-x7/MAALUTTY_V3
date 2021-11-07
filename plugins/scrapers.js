3b68be1a7e2b39b17efbf5cd0c55246daffacc50
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-11-07 19:51:04 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -1056,43 +1056,18 @@ else if (config.WORKTYPE == 'public') {
     Asena.addCommand({pattern: 'img ?(.*)', fromMe: false, desc: Lang.IMG_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
-        
-        var img_list = await WhatsAsenaStack.search_image(match[1])
-        await message.client.sendMessage(message.jid, Lang.IMG.format(5, match[1]), MessageType.text);
-        try {
-          var img1 = await axios.get(img_list.link1, {responseType: 'arraybuffer'})
-          await message.sendMessage(Buffer.from(img1.data), MessageType.image, { mimetype: Mimetype.png })
-        } catch {
-          return;
-        }
+        gis(match[1], async (error, result) => {
+            for (var i = 0; i < (result.length < 5 ? result.length : 5); i++) {
+                var get = got(result[i].url, {https: {rejectUnauthorized: false}});
+                var stream = get.buffer();
+                
+                stream.then(async (image) => {
+                    await message.client.sendMessage(message.jid,image, MessageType.image);
+                });
+            }
 
-        try {
-          var img2 = await axios.get(img_list.link2, {responseType: 'arraybuffer'})
-          await message.sendMessage(Buffer.from(img2.data), MessageType.image, { mimetype: Mimetype.png })
-        } catch {
-          return;
-        }
-
-        try {
-          var img3 = await axios.get(img_list.link3, {responseType: 'arraybuffer'})
-          await message.sendMessage(Buffer.from(img3.data), MessageType.image, { mimetype: Mimetype.png })
-        } catch {
-          return;
-        }
-
-        try {
-          var img4 = await axios.get(img_list.link4, {responseType: 'arraybuffer'})
-          await message.sendMessage(Buffer.from(img4.data), MessageType.image, { mimetype: Mimetype.png })
-        } catch {
-          return;
-        }
-      
-        try {
-          var img5 = await axios.get(img_list.link5, {responseType: 'arraybuffer'})
-          await message.sendMessage(Buffer.from(img5.data), MessageType.image, { mimetype: Mimetype.png })
-        } catch {
-          return;
-        }
+            message.reply(Lang.IMG.format((result.length < 5 ? result.length : 5), match[1]));
+        });
     }));
 
     Asena.addCommand({ pattern: 'github ?(.*)', fromMe: false, desc: Glang.GİTHUB_DESC }, async (message, match) => {
