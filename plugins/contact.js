0a2591797befd342fd15349bf3cec601ed6acec6
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-11-08 12:58:15 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -13,7 +13,6 @@ const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'ORG:Miss Helena;\n' // 
             + 'TEL;type=CELL;type=VOICE;waid=919605741366:+91 9605741366\n' // WhatsApp ID + phone number            
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "Miss Helena[OWNERS]", vcard: vcard}, MessageType.contact)
             + 'TEL;type=CELL;type=VOICE;waid=917994909899:+91 7994909899\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 await message.client.sendMessage(message.jid,{displayname: "Miss Helena[OWNERS]", vcard: vcard}, MessageType.contact)
