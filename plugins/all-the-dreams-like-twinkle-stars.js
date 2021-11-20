9ae4d050e4a77b676c89572bd8e74bba61962493
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-11-20 15:00:29 +0530

    Update aakalleeeeeeee

--- plugins/all-the-dreams-like-twinkle-stars.js
+++ plugins/all-the-dreams-like-twinkle-stars.js
@@ -20,8 +20,8 @@ let whb = Config.WORKTYPE == 'public' ? false : true
 Amalser.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a buttons message!credit Amal
     const buttons = [
-        {buttonId: 'id1', buttonText: {displayText: Config.AMAL_SER }, type: 1},
-        {buttonId: 'id2', buttonText: {displayText: Config.AMAL_S }, type: 1}
+        {buttonId: 'id1', buttonText: {displayText: Config.ALIVE_BUTTON }, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: Config.BUTTON_TWO }, type: 1}
       ]
       
       const buttonMessage = {
