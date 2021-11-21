07c4f42f6b267b9601d6a5630b9f119a94738944
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-11-21 20:36:28 +0530

    Create bgmlist.js

---
+++ plugins/bgmlist.js
@@ -0,0 +1,28 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const Config = require('../config');
+const fs = require('fs');
+const got = require('got');
+const config = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'list', fromMe: false, desc: 'command help menu'}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    r_text[0] = Config.LG_LOGO
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.jpg, contextInfo: {mentionedJid: message.mention}, caption: `
+   ` + config.LOGO_NAME + `
+
+🎗️ .whois
+💖 Displays metadata data of group or person._ 
+`}) 
+
+}));
