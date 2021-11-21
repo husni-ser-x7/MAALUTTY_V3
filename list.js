06d14db6d0a3499dfe08e2cf3bf2abaf548f9f8c
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-11-21 08:12:04 +0530

    Create list.js

---
+++ list.js
@@ -0,0 +1,123 @@
+☃️.owner
+🍒 get the original number of creator_ 
+☃️ .lyric 
+🍒 Finds the lyrics of the song._ 
+☃️ .covid 
+🍒 Shows the daily and overall covid table of more than 15 countries._ 
+☃️ .sweather 
+🍒 Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) for a p._ 
+☃️ .compliment 
+🍒 It sends complimentry sentenses_ 
+☃️ .hmod 
+🍒 Finds mod apps from happymod_ 
+☃️ .insult 
+🍒 It sends insulted words_ 
+☃️ .movie 
+🍒 Shows movie info._ 
+☃️ .joke 
+🍒 It sends random jokes_ 
+☃️ .vava 
+🍒 Chat with Pinky.
+ Use .molu <message>_ 
+☃️ .owner
+🍒 shows the detail of bot owner_ 
+☃️ .quote 
+🍒 It shares famous quotes_ 
+☃️ .ss 
+🍒 Takes a screenshot from the page in the given link._ 
+☃️ .setvar
+🍒 It sends high resolution wallpapers._ 
+☃️ .encrpt 
+🍒 Text encryption using base64._ 
+💬 Eg:- : .b64en <text>
+☃️ .show 
+🍒 Get info related to tv series and shows_ 
+☃️ .ig 
+🍒 Fetches user informations from instagram_ 
+☃️ .animesay 
+🍒 It writes the text inside the banner the anime girl is holding_ 
+☃️ .changesay 
+🍒 Turns the text into the change my mind poster._ 
+☃️ .trumpsay 
+🍒 Converts the text to Trump's tweet._ 
+☃️ .spdf 
+🍒 Converts a Site into PDF_ 
+☃️ .bgm 
+🍒 turn on and turn off bgm. -bot owner command_ 
+💬 Eg:- : .bgm on / off
+☃️ .autosticker 
+🍒 turn on and turn off bgm. -bot owner command_ 
+💬 Eg:- : .sticker on / off
+☃️ .sudo 
+🍒 changes sudo numbers_ 
+💬 Eg:- : .sudo *your number*
+☃️ .caption 
+🍒 changes all captions_ 
+💬 Eg:- : .caption *Made by JulieMwol*
+☃️ .number 
+🍒 change user number_ 
+💬 Eg:- : .number *Made by Amalser*
+☃️ .deployer 
+🍒 change user name_ 
+💬 Eg:- : .deployer *Made by Amalser*
+☃️ .handlers 
+🍒 changes handlers_ 
+💬 Eg:- : .handler ^[.!] 
+☃️ .botname 
+🍒 change your bot name_ 
+💬 Eg:- : .botname *name* 
+☃️ .theri  
+🍒 change your theri commands_ 
+💬 Eg:- : .theri command,command
+☃️ .sticker
+🍒 It converts your replied photo or video to sticker._ 
+☃️ .ffpack
+🍒 Free Fire logo maker_ 
+☃️ .ffpack
+🍒 Free Fire logo maker_ 
+☃️ .alive
+🍒 Does bot work?_ 
+☃️ .sysd
+🍒 Shows the system properties._ 
+☃️ .tagadmin
+🍒 Tags group admins._ 
+☃️ .bc
+🍒 Sends the replied message to all members in the group._ 
+☃️ .txtit
+🍒 Shows text to image tools with unlimited access._ 
+☃️ .antibadword 
+🍒 turn on & off anti-badword To remove members when they use bad words_ 
+💬 Eg:- : .antibadword on / off
+☃️ .a 
+🍒 Converts audio to sound recording._ 
+☃️ .unvoice
+🍒 Converts audio to sound recording._ 
+☃️ .update
+🍒 Checks the update._ 
+☃️ .update now
+🍒 It makes updates._ 
+☃️ .wallpaper
+🍒 It sends high resolution wallpapers._ 
+☃️ .wame 
+🍒 Get a link to the user chat._ 
+☃️ .rwarn
+🍒 Warning message 0_ 
+☃️ .weather 
+🍒 Shows the weather._ 
+☃️ .speedtest
+🍒 Measures Download and Upload speed._ 
+☃️ .ping
+🍒 Measures your ping._ 
+☃️ .short 
+🍒 Shorten the long link._ 
+☃️ .calc 
+🍒 Performs simple math operations._ 
+☃️ .sendi
+🍒 Download status from wa_ 
+☃️ .sendv
+🍒 Download status from wa_ 
+☃️ .whois
+🍒 Displays metadata data of group or person._ 
+`}) 
+
+}));t
