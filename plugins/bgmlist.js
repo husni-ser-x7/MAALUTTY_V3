d9975097a01635f03a25a0b337785c3f8b80ee84
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-11-21 22:21:08 +0530

    Create audio list

--- plugins/bgmlist.js
+++ plugins/bgmlist.js
@@ -8,7 +8,7 @@ const config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'list', fromMe: false, desc: 'command help menu'}, (async (message, match) => {
+Asena.addCommand({pattern: 'audio list', fromMe: false, desc: 'command help menu'}, (async (message, match) => {
 
     var r_text = new Array ();
     
@@ -21,8 +21,407 @@ Asena.addCommand({pattern: 'list', fromMe: false, desc: 'command help menu'}, (a
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.jpg, contextInfo: {mentionedJid: message.mention}, caption: `
    ` + config.LOGO_NAME + `
 
-🎗️ .whois
-💖 Displays metadata data of group or person._ 
+💎 Sinan
+
+💎 Dud
+
+💎 Photo
+
+💎 fan
+
+💎 Gd nyt
+
+💎 koi
+
+💎 Vatt
+
+💎 ara
+
+💎 Ara
+
+💎 Name
+
+💎 pinnalla
+
+💎 Hai
+
+💎 Reply
+
+💎 Thall
+
+💎 Song
+
+💎 kozhi
+
+💎 I love you
+
+💎 line
+
+💎 mass
+
+💎 arulle
+
+💎 Mirash
+
+💎 Mirash bgm
+
+💎 Salman
+
+💎 Arshi
+
+💎 Ezra
+
+💎 nihal
+
+💎 sticker
+
+💎 sufaid
+
+💎 jinn
+
+💎 Feel
+
+💎 lub
+
+💎 foxy
+
+💎 Music
+
+💎 CR7
+
+💎 Alan
+
+💎 Halo
+
+💎 Aysheri
+
+💎 kooi
+
+💎 Achumon
+
+💎 ith
+
+💎 bye
+
+💎 Para
+
+💎 Khais
+
+💎 mukesh
+
+💎 Hlo
+
+💎 njan
+
+💎 Njan
+
+💎 muthe
+
+💎 Xxxtentacion
+
+💎 alone
+
+💎 Converting
+
+💎 potta
+
+💎 menu
+
+💎 welcome
+
+💎 vannu
+
+💎 Remove
+
+💎 kyat
+
+💎 rip
+
+💎 remix
+
+💎 Myr
+
+💎 dj
+
+💎 pever
+
+💎 Oo
+
+💎 XXXTENTACION
+
+💎 Love u
+
+💎 hate
+
+💎 Njan vannu
+
+💎 tto
+
+💎 Audio
+
+💎 veno
+
+💎 xxxtentacion
+
+💎 nanban
+
+💎 Ayye
+
+💎 Ronaldo
+
+💎 Kundan
+
+💎 katha
+
+💎 hy
+
+💎 vaanam
+
+💎 Vanam
+
+💎 Ivan
+
+💎 Annan
+
+💎 Thayoli
+
+💎 umbi
+
+💎 Noob
+
+💎 Single
+
+💎 Nanban
+
+💎 Achu
+
+💎 Ok
+
+💎 Ameen
+
+💎 Shiva
+
+💎 ok
+
+💎 Enth
+
+💎 ameen
+
+💎 enth
+
+💎 Edi
+
+💎 Uff
+
+💎 Dey
+
+💎 Cr7
+
+💎 Poda
+
+💎 Poli
+
+💎 Pm
+
+💎 Plzz
+
+💎 girl
+
+💎 Oi
+
+💎 mp3
+
+💎 nirth
+
+💎 good nyt
+
+💎 birthday
+
+💎 Pattula
+
+💎 Good night
+
+💎 Mood
+
+💎 Please
+
+💎 Mm
+
+💎 Dance
+
+💎 Ith
+
+💎 thech
+
+💎 caption
+
+💎 Polikk
+
+💎 Aah
+
+💎 Mrng
+
+💎 alive
+
+💎 Di
+
+💎 Bro
+
+💎 avan
+
+💎 Allah
+
+💎 bg
+
+💎 help
+
+💎 Hii
+
+💎 odiko
+
+💎 oho
+
+💎 killadi
+
+💎 oo
+
+💎 Fuck
+
+💎 invite
+
+💎 Hi
+
+💎 love
+
+💎 Bgm
+
+💎 Owner
+
+💎 Group
+
+💎 list
+
+💎 man
+
+💎 Nanba
+
+💎 oii
+
+💎 poda
+
+💎 yo
+
+💎 sad
+
+💎 work
+
+💎 set
+
+💎 remove
+
+💎 thukum
+
+💎 poko
+
+💎 kundan
+
+💎 xmedia
+
+💎 commands
+
+💎 Boss
+
+💎 vava
+
+💎 Myre
+
+💎 Lalitha
+
+💎 start
+
+💎 sing
+
+💎 Hello
+
+💎 Bye
+
+💎 warn
+
+💎 Baby
+
+💎 who
+
+💎 find
+
+💎 power
+
+💎 myr
+
+💎 vanam
+
+💎 tagall
+
+💎 Re entry
+
+💎 pass
+
+💎 patti
+
+💎 phaa
+
+💎 mone
+
+💎 look
+
+💎 bor
+
+💎 try
+
+💎 owner
+
+💎 luv
+
+💎 feel
+
+💎 romam
+
+💎 potte
+
+💎 no replay
+
+💎 bgm
+
+💎 vijay
+
+💎 poli
+
+💎 Da
+
+💎 Vazha
+
+💎 kunna
+
+💎 Sorry
+
+💎 Bot
+
+💎 song
+
+💎 Uyir
+
+💎 myre
+
+💎 thayoli
+
+💎 Love
+
+💎 Hy
+
+💎 Sed
+
+💎 Sad
+
+💎 Ayn
 `}) 
 
 }));
