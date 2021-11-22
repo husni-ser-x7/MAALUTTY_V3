daf9ea829ff4f672646250e98143c25c3049fb7e
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-11-23 00:33:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ module.exports = {
     THERI_KICK: process.env.THERI_KICK === undefined ? 'false' : process.env.THERI_KICK,
     SONGD: process.env.SONGD === undefined ? 'ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ꜱᴏɴɢ' : process.env.SONGD,
     SONGU: process.env.SONGU === undefined ? 'ᴜᴘʟᴏᴀᴅɪɴɢ ꜱᴏɴɢ' : process.env.SONGU,
-    TALKING_HELENA: process.env.TALKING_HELENA === undefined ? 'false' : process.env.TALKING_HELENA,
+    CHATBOT: process.env.CHAT_BOT === undefined ? 'false' : process.env.CHAT_BOT,
     SAID: process.env.BGM_DURATION === undefined ? '39999600' : process.env.BGM_DURATION,
     BLOCKMSG: process.env.BLOCK_MESSAGE === undefined ? 'default' : process.env.BLOCK_MESSAGE,
     UNBLOCKMSG: process.env.UNBLOCK_MESSAGE === undefined ? 'default' : process.env.UNBLOCK_MESSAGE,
