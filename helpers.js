c2bcb41b6fee2adce52ea9bd9872985a470b90f9
Husni and nijin <93671844+nijin-husni@users.noreply.github.com>
2021-12-09 19:06:21 +0530

    💞NIJIN HUSNI💞 ORU SEMBAVAM THANNE 😂

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 /*
-# Copyright (C) 2020 MuhammedKpln.
+# Copyright (C) 2020 nijin-husni.
 #
 # WhatsAsena is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Miss Helena*:  ```" + msg + "```"
+    return "✅ *Maalutty*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Miss Helena*:  ```" + msg + "```"
+    return "🛑 *Maalutty*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Miss Helena*:  ```" + msg + "```"
+    return "⏺️ *Maalutty*:  ```" + msg + "```"
 }
 
 
