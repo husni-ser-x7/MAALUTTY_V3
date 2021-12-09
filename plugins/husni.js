836d91d076d3fbec14c29e910f0b43eaad1ef22b
Husni and nijin <93671844+nijin-husni@users.noreply.github.com>
2021-12-09 22:22:51 +0530

    💞NIJIN HUSNI💞 ORU SEMBAVAM THANNE 😂

--- plugins/husni.js
+++ plugins/husni.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Amalser.
+/* Copyright (C) 2020 nijin-husni.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Amalser - Amal,farhan
+nijin-husni - husni,nijin
 */
 
 const Asena = require('../events');
