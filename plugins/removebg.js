04eccebb489eeeb0c7c54677df32db3e51db2af2
Husni and nijin <93671844+nijin-husni@users.noreply.github.com>
2021-12-10 21:41:21 +0530

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -88,7 +88,7 @@ else if (Config.WORKTYPE == 'public') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'Miss Helena.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'Maalutty.png', mimetype: Mimetype.png});
         await load.delete();
     }));
     
