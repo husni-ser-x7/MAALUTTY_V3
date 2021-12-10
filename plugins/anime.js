2395024dfae505cad90b35f29de46b6202ffb2f7
Husni and nijin <93671844+nijin-husni@users.noreply.github.com>
2021-12-10 21:42:53 +0530

    •━╼⤏💝 MAALUTTY_V3💝⤎╾━•

--- plugins/anime.js
+++ plugins/anime.js
@@ -94,7 +94,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by Miss Helena'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Maalutty_v3'})
 
     }));
 }
