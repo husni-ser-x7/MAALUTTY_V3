4f0633a029ebd0debdccd463a76763260a8e9890
Husni and nijin <93671844+nijin-husni@users.noreply.github.com>
2021-12-15 08:37:05 +0530

    Update mforward.js

--- plugins/mforward.js
+++ plugins/mforward.js
@@ -1,7 +1,7 @@
-/* coded by husni and nijin
+/* coded by raashii
 */
 
- const Maalutty = require('../events');
+ const Rashi = require('../events');
  const { MessageType, Mimetype } = require('@adiwajshing/baileys');
  const fs = require('fs');
  const ffmpeg = require('fluent-ffmpeg');
@@ -9,18 +9,18 @@
  const Config = require('../config');
 
           var mode = ''
-if (Config.OWNER == 'default') mode = 'ᴅᴇᴠᴇʟᴏᴘᴇʀ : ʀᴀᴀsʜɪɪ'
+if (Config.PLK == 'default') mode = 'ᴅᴇᴠᴇʟᴏᴘᴇʀ : ʀᴀᴀsʜɪɪ'
 
-else mode = 'ғᴏʀᴡᴀʀᴅᴇᴅ ʙʏ ' + Config.OWNER
+else mode = 'ғᴏʀᴡᴀʀᴅᴇᴅ ʙʏ ' + Config.PLK
 
-            const maalu = {
+            const raashi = {
 
                   key: {"fromMe": false,"participant": "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "62895619083555-1616169743@g.us","inviteCode": "mememteeeekkeke","groupName": "Elsa Mwol", "caption": mode , 'jpegThumbnail': fs.readFileSync('./media/image/logo.jpg')}}
              
              
             }
 
-Maalutty.addCommand({pattern: 'mforward ?(.*)', fromMe: true, desc: 'its foraward replied audio'}, (async (message, match) => {   
+Rashi.addCommand({pattern: 'mforward ?(.*)', fromMe: true, desc: 'its foraward replied audio'}, (async (message, match) => {   
   
     if (message.reply_message === false);
     
@@ -36,9 +36,9 @@ Maalutty.addCommand({pattern: 'mforward ?(.*)', fromMe: true, desc: 'its forawar
     });
 let id = match[1];
     ffmpeg(location)
-        .format('mp3')
-        .save('output.mp3')
+        .format('mp4')
+        .save('output.mp4')
         .on('end', async () => {
-            await message.client.sendMessage(_.jid, buff, audio, { quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})}, message: { orderMessage: { itemCount: 9999999999, status: 200, thumbnail: fs.readFileSync('./photo/maalutty.jpg'), surface: 200, message: `${bc}`, orderTitle: `${bc}`, sellerJid: '0@s.whatsapp.net'}}}, contextInfo: { forwardingScore: 508, isForwarded: true}, mimetype: 'audio/mp4', duration: 7356663773663, ptt: true })
+            await message.client.sendMessage(id, fs.readFileSync('output.mp4'), MessageType.video, {mimetype: Mimetype.mp4,contextInfo: { forwardingScore: 2, isForwarded: true }});
 });
 }));
