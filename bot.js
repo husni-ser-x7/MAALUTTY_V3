4a8604f2db9b67bbbd2ac1600c114ade3e1f4f36
Husni and nijin <93671844+nijin-husni@users.noreply.github.com>
2022-01-02 14:43:05 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -143,7 +143,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
                 await git.fetch();
                 var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
                 if (commits.total === 0) {
-                    await conn.sendMessage(conn.user.jid, fs.readFileSync("./photo/maalutty.jpg"), MessageType.image, { caption: `*ʜᴇʏ ʙʀᴏ ᴍᴀᴀʟᴜᴛᴛʏ_ᴠ3 ᴡᴏʀᴋɪɴɢ ` + config.WORKTYPE + ` 🥂* \n\n*.ᴏᴡɴᴇʀᴄᴍᴅ ɢᴇᴛ ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅs🔰* \n\n*ᴛʜᴀɴᴋʏᴏᴜ ғᴏʀ ᴜsɪɴɢ ᴍᴀᴀʟᴜᴛᴛʏ_ᴠ3🥂*`});
+                    await conn.sendMessage(conn.user.jid,Lang.UPDATE, MessageType.text);
                 } else {
                     var degisiklikler = Lang.NEW_UPDATE;
                     commits['all'].map(
